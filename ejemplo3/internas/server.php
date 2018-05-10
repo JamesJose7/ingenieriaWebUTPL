e5fcdd6ddfe40261631f65f8be13e74914be7425
Jose Eduardo <pepe.96.jose@gmail.com>
2018-05-10 16:57:46 -0500

    Mejora de presentacion en factura

--- ejemplo3/internas/server.php
+++ ejemplo3/internas/server.php
@@ -6,7 +6,7 @@ $costo_curso = 80;
 $costo_taller =10;
 
 $descuento_docente = 0.1;
-$descuento_estudiante = 0.25;
+$descuento_estudiante = 0.20;
 
 // factura
 
@@ -37,7 +37,27 @@ if ($tipo == "docente") {
 	$costo_total = $costo_total - ($costo_total * $descuento_estudiante);
 }
 
-echo "Nombre: ".$nombres." ".$apellidos."<br>";
-echo "Precio total: ".$costo_total;
+print "<h2>FACTURA</h2><br>==============================================<br>";
+echo "<strong>CI: </strong>".$cedula."<br>";
+echo "<strong>Nombre: </strong>".$nombres." ".$apellidos."<br>";
+echo "<strong>Fecha: </strong>".$fecha."<br>";
+
+//Cursos inscritos
+if (isset($curso) && !empty($curso)) {
+	print "<h3>Cursos</h3>=============<br>";
+    echo $curso." <strong>&nbsp;&nbsp;&nbsp;&nbsp;+$80</strong><br>";
+}
+
+// Talleres inscritos
+if (isset($talleres) && !empty($talleres)) {
+	print "<h3>Talleres</h3>=============<br>";
+    $talleres = $_POST['talleres'];
+
+	for ($i=0; $i<count($talleres); $i++) {
+	    echo $talleres[$i]." <strong>&nbsp;&nbsp;&nbsp;&nbsp;+$10</strong><br>";
+	}
+}
+
+echo "<br><br><br><strong>Precio total: </strong>$".$costo_total;
 
 ?>

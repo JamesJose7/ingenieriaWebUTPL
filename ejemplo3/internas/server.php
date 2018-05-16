a2aa4c1fbdc9eca89f5e79755b92d7e2af992c47
Jose Eduardo <pepe.96.jose@gmail.com>
2018-05-16 18:22:41 -0500

    Styles added for receipt

--- ejemplo3/internas/server.php
+++ ejemplo3/internas/server.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Factura | TEC-UTPL 2018-</title>
+	
+	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/form.css">
+</head>
+<body>
+
 <?php
 extract($_POST);
 
@@ -7,6 +20,9 @@ $costo_taller =10;
 
 $descuento_docente = 0.1;
 $descuento_estudiante = 0.20;
+
+date_default_timezone_set('America/Los_Angeles');
+$fecha_actual = date('m/d/Y', time());
 
 // factura
 
@@ -36,28 +52,64 @@ if ($tipo == "docente") {
 } else if ($tipo == "estudiante") {
 	$costo_total = $costo_total - ($costo_total * $descuento_estudiante);
 }
-
-print "<h2>FACTURA</h2><br>==============================================<br>";
-echo "<strong>CI: </strong>".$cedula."<br>";
-echo "<strong>Nombre: </strong>".$nombres." ".$apellidos."<br>";
-echo "<strong>Fecha: </strong>".$fecha."<br>";
-
-//Cursos inscritos
-if (isset($curso) && !empty($curso)) {
-	print "<h3>Cursos</h3>=============<br>";
-    echo $curso." <strong>&nbsp;&nbsp;&nbsp;&nbsp;+$80</strong><br>";
-}
-
-// Talleres inscritos
-if (isset($talleres) && !empty($talleres)) {
-	print "<h3>Talleres</h3>=============<br>";
-    $talleres = $_POST['talleres'];
-
-	for ($i=0; $i<count($talleres); $i++) {
-	    echo $talleres[$i]." <strong>&nbsp;&nbsp;&nbsp;&nbsp;+$10</strong><br>";
-	}
-}
-
-echo "<br><br><br><strong>Precio total: </strong>$".$costo_total;
-
 ?>
+
+
+<!--******** Factura ********-->
+<section id="factura">
+<header>
+	<h1>FACTURA</h1>
+	<p><span>CI: </span><?php echo $cedula ?></p>
+	<p><span>Nombre: </span><?php echo $nombres." ".$apellidos ?></p>
+	<p><span>Fecha: </span><?php echo $fecha_actual ?></p>
+</header>
+
+<main>
+<!-- Cursos inscritos -->
+<?php if (isset($curso) && !empty($curso)) {?>
+	<table>
+		<tr>
+			<th colspan="2">Cursos</th>
+		</tr>
+		<tr>
+			<th>Item</th>
+			<th>Precio</th>
+		</tr>
+		<tr>
+			<td><?php echo $curso ?></td>
+			<td>+$80</td>
+		</tr>
+	</table>
+<?php }?>
+
+<!-- Talleres inscritos -->
+<?php if (isset($talleres) && !empty($talleres)) {?>
+	<table>
+		<tr>
+			<th colspan="2">Talleres</th>
+		</tr>
+		<tr>
+			<th>Item</th>
+			<th>Precio</th>
+		</tr>
+
+    <?php $talleres = $_POST['talleres'];
+
+	for ($i=0; $i<count($talleres); $i++) {?>
+		<tr>
+			<td><?php echo $talleres[$i] ?></td>
+			<td>+$10</td>
+		</tr>
+	<?php }?>
+	</table>
+<?php }?>
+
+</main>
+
+<footer>
+	<p><span>Precio total: </span>$<?php echo $costo_total ?></p>
+</footer>
+</section>
+
+</body>
+</html>
